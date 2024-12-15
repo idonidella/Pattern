9eb39122b478888108b5aeffa07d228121f6a135
Efekan Efe <efekanefe63186@gmail.com>
2024-12-15 23:37:40 +0300

    Refactor code for clarity: update comments to English, improve formatting, and enhance readability in app.py and model.py

--- model/app.py
+++ model/app.py
@@ -7,11 +7,11 @@ import io
 import base64
 from flask_cors import CORS
 
-# Flask application setup
-app = Flask(__name__)
-CORS(app)  # Enable CORS support for frontend
 
-# Load pre-trained MNIST model
+app = Flask(__name__)
+CORS(app)
+
+
 model = load_model('mnist_cnn_model_0.keras')
 
 # Home route
@@ -24,41 +24,39 @@ def base():
     print(request)
     return 'MNIST Model API'
 
-# Recognize endpoint to handle image input and make predictions
+
 @app.route('/recognize', methods=['POST'])
 def recognize():
     try:
-        # Get the image in base64 format from the POST request
+
         img_data = request.json['image']
         
-        # Decode the base64 image data
-        img_data = img_data.split(',')[1]  # Ignore the initial part of the base64 string
+        img_data = img_data.split(',')[1]  
         img = Image.open(io.BytesIO(base64.b64decode(img_data)))
         
-        # Ensure the image is grayscale
-        img = img.convert('L')  # Convert to grayscale
-        img = img.resize((28, 28))  # Resize to 28x28 pixels (MNIST input size)
+
+        img = img.convert('L')  
+        img = img.resize((28, 28))  
         
         # Convert image to a numpy array and normalize the pixel values
         img = np.array(img) / 255.0  # Normalize the image data
         img = img.reshape(1, 28, 28, 1)  # Reshape for the model input (28, 28, 1)
 
-        # Debugging: Print shape and type of image data
+
         print(f"Image shape: {img.shape}, Image dtype: {img.dtype}")
 
-        # Make prediction
+
         prediction = model.predict(img)
 
-        # Get the predicted class and its confidence level
+
         predicted_class = np.argmax(prediction, axis=1)[0]
         predicted_confidence = np.max(prediction)
 
-        # Return the prediction and confidence in JSON format
         return jsonify({'prediction': str(predicted_class), 'confidence': str(predicted_confidence)})
 
     except Exception as e:
-        # If any error occurs, return the error message
-        print(f"Error: {str(e)}")  # Log the error in the backend
+
+        print(f"Error: {str(e)}")  
         return jsonify({'error': f"Error during prediction: {str(e)}"})
 
 if __name__ == '__main__':
